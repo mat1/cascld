edefcfe6e565b8f77f2151923ccb0fc8838bba3e
Matthias Brun <matthias.brun@gmail.com>
2025-07-11 15:04:54 +0200

    Terraform intro

--- iac/intro/learn-terraform-gcp/main.tf
+++ iac/intro/learn-terraform-gcp/main.tf
@@ -8,11 +8,29 @@ terraform {
 }
 
 provider "google" {
-  project = "cas-terraform-intro"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
