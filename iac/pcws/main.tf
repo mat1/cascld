505887546d6111671be0c592afe9bd240461876d
Matthias Brun <matthias.brun@gmail.com>
2025-07-18 15:17:52 +0200

    Create storage bucket

--- iac/pcws/main.tf
+++ iac/pcws/main.tf
@@ -18,6 +18,8 @@ resource "google_cloud_run_v2_service" "pcws" {
   ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
+    service_account = google_service_account.pcws.email
+
     containers {
       image = "gcr.io/${var.project}/pcws:latest"
 
@@ -30,7 +32,7 @@ resource "google_cloud_run_v2_service" "pcws" {
         value = "true"
       }
       env {
-        name = "USE_ACCOUNT_SERVICE_MOCK"
+        name  = "USE_ACCOUNT_SERVICE_MOCK"
         value = "true"
       }
       env {
@@ -45,14 +47,46 @@ resource "google_cloud_run_v2_service" "pcws" {
         name  = "PCWS_DATASOURCE_PASSWORD"
         value = var.pcws_password
       }
+
+      volume_mounts {
+        mount_path = "/mnt/pcws-bucket"
+        name       = "pcws-bucket"
+      }
     }
+
     vpc_access {
       network_interfaces {
         network    = var.vpc_name
         subnetwork = var.vpc_name
       }
     }
+
+    volumes {
+      name = "pcws-bucket"
+      gcs {
+        bucket    = google_storage_bucket.pcws_bucket.name
+        read_only = false # Set to false to allow writing
+      }
+    }
   }
+}
+
+# Create a bucket
+resource "google_storage_bucket" "pcws_bucket" {
+  name     = "${var.project}-pcws-bucket"
+  location = var.region
+}
+
+# Create service account and allow write access
+resource "google_service_account" "pcws" {
+  account_id   = "pcws-sa"
+  display_name = "PCWS Service Account"
+}
+
+resource "google_storage_bucket_iam_member" "cloud_run_write" {
+  bucket = google_storage_bucket.pcws_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.pcws.email}"
 }
 
 # Allow unauthenticated access
