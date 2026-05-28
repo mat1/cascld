9314ea0706a0d858cfdaf66a8e6875c630d8004b
Matthias Brun <matthias.brun@dsiag.ch>
2026-05-28 16:33:52 +0200

    Update cloud nuke

--- iaas-lab/prepare/nuke-parallel.sh
+++ iaas-lab/prepare/nuke-parallel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 1. Configure aws access key and security key for aws cli: aws configure
+
 # List of all AWS regions
 aws_regions=(
     "us-east-1" "us-east-2" "us-west-1" "us-west-2" "af-south-1" "ap-east-1"
